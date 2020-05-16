9c136e1db6260b712208cfd648f98b7bb7d3b7c0
FFYzz <cryptochen95@gmail.com>
2020-05-16 18:24:12 +0800

    328. Odd Even Linked List

---
+++ problem-set/Linked-List/328.-Odd-Even-Linked-List/OddEvenLinkedList.java
@@ -0,0 +1,42 @@
+/**
+ * @Title: 328. Odd Even Linked List
+ * @Author: FFYzz
+ * @Mail: cryptochen95 at gmail dot com
+ * @Date: 2020/5/16
+ */
+public class OddEvenLinkedList {
+
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode odd = head, even = head.next, evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
