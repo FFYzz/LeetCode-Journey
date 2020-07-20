3f67a4c1f75b9919de6d015f785091aa3d641af3
FFYzz <cryptochen95@gmail.com>
2020-07-20 22:38:53 +0800

    518. Coin Change 2

---
+++ problem-set/Dynamic-Programming/518.-Coin-Change-2/CoinChange2.java
@@ -0,0 +1,21 @@
+/**
+ * @Title: 518. Coin Change 2
+ * @Author: FFYzz
+ * @Mail: cryptochen95 at gmail dot com
+ * @Date: 2020/7/15
+ */
+public class CoinChange2 {
+
+    public int change(int amount, int[] coins) {
+        int[][] dp = new int[coins.length + 1][amount + 1];
+        dp[0][0] = 1;
+        for (int i = 1; i <= coins.length; ++i) {
+            dp[i][0] = 1;
+            for (int j = 1; j <= amount; ++j) {
+                dp[i][j] = dp[i - 1][j] + (j >= coins[i - 1] ? dp[i][j - coins[i - 1]] : 0);
+            }
+        }
+        return dp[coins.length][amount];
+    }
+
+}
